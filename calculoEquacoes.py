722f9e815ee3595082fdc5a94dae35363b1d4e21
Kayke <kayke.gsousa15@gmail.com>
2025-01-28 11:42:10 -0300

    Initial commit

---
+++ calculoEquacoes.py
@@ -0,0 +1,6 @@
+def areaTriangulo(a, b):
+    area = (a * b) / 2
+    return area
+
+def perimetroTriangulo(a, b, c):
+    return a + b + c
---
+++ calculoSimples.py
@@ -0,0 +1,16 @@
+def multiplicacao(a, b):
+    return  a * b
+
+def adicao(a, b):
+    return  a + b
+
+def subtracao(a, b):
+    return  a - b
+
+def divisao(a, b):
+    if a or b == 0:
+        return "Erro. Divisão por 0 não pode ser realizada."
+    else:
+        return  a / b
+
+  
